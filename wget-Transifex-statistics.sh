44928b0844a835e75077dfd7e2f4bf119478f925
Christoph Scheid <c@shri.de>
2012-11-06 10:39:20 +0100

    github webhook testing...

--- wget-Transifex-statistics.sh
+++ wget-Transifex-statistics.sh
@@ -7,6 +7,8 @@
 # There should be a cronjob running on the production system
 # running this script once per hour or daily or so...
 #
+# add one to call this script once in a while: "crontab -e" 
+#
 # keep the old version and give it a filename containing a date stamp 
 mv c3sintent/static/C3Sintent-transifex-languages.png  c3sintent/static/C3Sintent-transifex-languages-`date "+%Y-%m-%d-%H%M"`.png
 # get a more recent version
